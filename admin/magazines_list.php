43c31b8f1884af0c24ff788c81fcb90196cd930c
artur234 <jaskier234@gmail.com>
2020-01-25 13:49:31 +0100

    Added showing capacity in magazine list

--- admin/magazines_list.php
+++ admin/magazines_list.php
@@ -8,10 +8,10 @@ require '../database.php';
 require '../show_table.php';
 
 // no need for preparing this query
-$result = Database::get_query_result("SELECT depot_id, address FROM depot");
+$result = Database::get_query_result("SELECT d.depot_id, d.address, SUM(COALESCE(p.quantity, 0)) / d.capacity FROM depot d LEFT JOIN product p ON p.depot = d.depot_id GROUP BY d.depot_id ORDER BY d.depot_id");
 
 $magazines_list = pg_fetch_all($result, PGSQL_NUM);
 
-show_array_as_table($magazines_list, array(0, 1));
+show_array_as_table($magazines_list, array(0, 1, 2));
 
  ?>
