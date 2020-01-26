5b4d8017a116a9042533f988967e02a492910c32
artur234 <jaskier234@gmail.com>
2020-01-26 20:02:54 +0100

    FIxed bug in authenticate

--- authenticate.php
+++ authenticate.php
@@ -9,8 +9,12 @@ function failure() {
 }
 
 function authenticate($mode, $user_id = -1) {
+    
+    // var_dump($_SESSION['user_id']);
+    // var_dump($_SESSION['is_admin']);
+    
     if ($mode === 'admin'){
-        if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] === false) { // only admin
+        if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] === 'f') { // only admin
             failure();
         }
     } else if ($mode === 'user' && $user_id === -1) { // Every logged user
@@ -18,7 +22,7 @@ function authenticate($mode, $user_id = -1) {
             failure();
         }
     } else if ($mode === 'user/admin' && $user_id !== -1) { // only user with certain id or admin
-        if ((!isset($_SESSION['user_id']) || srtval($_SESSION['user_id']) !== $user_id) && $_SESSION['is_admin'] === false) {
+        if ((!isset($_SESSION['user_id']) || strval($_SESSION['user_id']) !== $user_id) && $_SESSION['is_admin'] === false) {
             failure();
         }
     } else { // TODO? only user with certain id (not admin)
