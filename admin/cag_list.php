6af97af62abac398f23fc72fe8d1a7ca5a7031b5
artur234 <jaskier234@gmail.com>
2020-01-26 14:23:14 +0100

    Change in cag list sql query

--- admin/cag_list.php
+++ admin/cag_list.php
@@ -4,10 +4,10 @@ require '../database.php';
 require '../show_table.php';
 
 // no need for preparing this query
-$result = Database::get_query_result("SELECT cag_id, address FROM collect_and_go");
+$result = Database::get_query_result("SELECT cag_id, address, used FROM collect_and_go");
 
 $cag_list = pg_fetch_all($result, PGSQL_NUM);
 
-show_array_as_table($cag_list, array(0, 1));
+show_array_as_table($cag_list, array(0, 1, 2));
 
  ?>
