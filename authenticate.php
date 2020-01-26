93f8645bc68770f1d85999ae3664741b18221ebe
artur234 <jaskier234@gmail.com>
2020-01-26 17:32:04 +0100

    Added some auhtentication modes

--- authenticate.php
+++ authenticate.php
@@ -4,21 +4,21 @@ session_start();
 
 function authenticate($mode, $user_id = -1) {
     if ($mode === 'admin'){
-        if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] === false) {
+        if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] === false) { // only admin
             echo "Musisz być zalogowany";
             header("HTTP/1.0 403 Not Found");
         }
-    } else if ($mode === 'user' && $user_id === -1) {
+    } else if ($mode === 'user' && $user_id === -1) { // Every logged user
         if (!isset($_SESSION['user_id'])) {
             echo "Musisz być zalogowany";
             header("HTTP/1.0 403 Not Found");
         }
-    } else if ($mode === 'user' && $user_id !== -1) {
-        if (!isset($_SESSION['user_id']) || srtval($_SESSION['user_id']) !== $user_id) {
+    } else if ($mode === 'user/admin' && $user_id !== -1) { // only user with certain id or admin
+        if ((!isset($_SESSION['user_id']) || srtval($_SESSION['user_id']) !== $user_id) && $_SESSION['is_admin'] === false) {
             echo "Musisz być zalogowany";
             header("HTTP/1.0 403 Not Found");
         }
-    } else {
+    } else { // TODO? only user with certain id (not admin)
         echo "Musisz być zalogowany";
         header("HTTP/1.0 403 Not Found");
     }
