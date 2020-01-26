61f0d7dd979467204b573ec7b30856a83858dab3
artur234 <jaskier234@gmail.com>
2020-01-26 14:48:01 +0100

    Added constraints to some database values

--- initialize_database.sql
+++ initialize_database.sql
@@ -9,10 +9,10 @@ CREATE TABLE orders (
     order_id SERIAL PRIMARY KEY,
     user_name INT,
     order_date TIMESTAMP,
-    due_date TIMESTAMP,
-    time_window_length FLOAT,
-    price NUMERIC(9,2),
-    address VARCHAR(100)
+    due_date TIMESTAMP NOT NULL,
+    time_window_length FLOAT NOT NULL,
+    price NUMERIC(9,2) NOT NULL,
+    address VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE orders_archive (
@@ -30,7 +30,7 @@ CREATE TABLE product (
     product_name VARCHAR(255),
     quantity FLOAT CHECK (quantity >= 0),
     unit CHAR(4) NOT NULL, -- TODO check if value is correct szt./kg.
-    price NUMERIC(9,2),
+    price NUMERIC(9,2) NOT NULL,
     depot INT
 );
 
@@ -57,6 +57,6 @@ CREATE TABLE depot (
 CREATE TABLE collect_and_go (
     cag_id SERIAL PRIMARY KEY,
     address VARCHAR(255),
-    capacity INT,
+    capacity INT NOT NULL,
     used INT CHECK (used <= capacity) -- Also contains orders ordered but not in C&G point
 );
